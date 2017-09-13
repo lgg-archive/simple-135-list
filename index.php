fd19b99e3db36770deffbc5d5e71189dae59e741
littleguga <fed777os@gmail.com>
2017-09-14 01:12:09 +0300

    fix https link

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <meta charset="utf-8"/>
 <title>Список дел на сегодня</title>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.8.3.js"></script>
 <script type="text/javascript" src="md5.js"></script>
 <script type="text/javascript" src="js.js"></script>
 <link href="style.css" rel="stylesheet" type="text/css"/>
@@ -38,7 +38,7 @@
 		<div class="workline">
 			<input type="checkbox" class="check" id="w1c" onclick="markthis('w1c','w1');"/>
 			<label for="w1c"></label>
-			<img src="redline.jpg" class="redline" id="rl1"/>
+			<img src="./img/redline.jpg" class="redline" id="rl1"/>
 			<input type="text" class="work1" id="w1" OnKeyUp="savethis(this);">
 		</div>
 	</div>
@@ -46,19 +46,19 @@
 		<div class="workline">
 			<input type="checkbox" class="check" id="w31c" onclick="markthis('w31c','w31');">
 			<label for="w31c"></label>
-			<img src="redline.jpg" class="redline" id="rl31"/>
+			<img src="./img/redline.jpg" class="redline" id="rl31"/>
 			<input type="text" class="work3" id="w31" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w32c" onclick="markthis('w32c','w32');">
 			<label for="w32c"></label>
-			<img src="redline.jpg" class="redline" id="rl32"/>
+			<img src="./img/redline.jpg" class="redline" id="rl32"/>
 			<input type="text" class="work3" id="w32" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w33c" onclick="markthis('w33c','w33');">
 			<label for="w33c"></label>
-			<img src="redline.jpg" class="redline" id="rl33"/>
+			<img src="./img/redline.jpg" class="redline" id="rl33"/>
 			<input type="text" class="work3" id="w33" OnKeyUp="savethis(this);">
 		</div>
 	</div>
@@ -66,31 +66,31 @@
 		<div class="workline">
 			<input type="checkbox" class="check" id="w51c" onclick="markthis('w51c','w51');">
 			<label for="w51c"></label>
-			<img src="redline.jpg" class="redline" id="rl51"/>
+			<img src="./img/redline.jpg" class="redline" id="rl51"/>
 			<input type="text" class="work5" id="w51" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w52c" onclick="markthis('w52c','w52');">
 			<label for="w52c"></label>
-			<img src="redline.jpg" class="redline" id="rl52"/>
+			<img src="./img/redline.jpg" class="redline" id="rl52"/>
 			<input type="text" class="work5" id="w52" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w53c" onclick="markthis('w53c','w53');">
 			<label for="w53c"></label>
-			<img src="redline.jpg" class="redline" id="rl53"/>
+			<img src="./img/redline.jpg" class="redline" id="rl53"/>
 			<input type="text" class="work5" id="w53" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w54c" onclick="markthis('w54c','w54');">
 			<label for="w54c"></label>
-			<img src="redline.jpg" class="redline" id="rl54"/>
+			<img src="./img/redline.jpg" class="redline" id="rl54"/>
 			<input type="text" class="work5" id="w54" OnKeyUp="savethis(this);">
 		</div>
 		<div class="workline">
 			<input type="checkbox" class="check" id="w55c" onclick="markthis('w55c','w55');">
 			<label for="w55c"></label>
-			<img src="redline.jpg" class="redline" id="rl55"/>
+			<img src="./img/redline.jpg" class="redline" id="rl55"/>
 			<input type="text" class="work5" id="w55" OnKeyUp="savethis(this);">
 		</div>
 	</div>
